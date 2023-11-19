087d75611e70bd69d7c145f10d473aa90c80ca20
Ricgo01 <128996354+Ricgo01@users.noreply.github.com>
2023-11-18 21:45:07 -0600

    Update DriverContenedor.java

--- redcycle/src/main/java/Controlador/DriverContenedor.java
+++ redcycle/src/main/java/Controlador/DriverContenedor.java
@@ -19,8 +19,15 @@ import java.util.ArrayList;
 import java.util.Date;
 
 /**
+ * POO 2 Semestre
+ * Facultad de Ingenieria
+ * Departamento de Ciencias de la Computacion
+ * Fecha inicio: 19/09/2023
+ * Fecha final: 19/09/2023
  *
- * @author nadis
+ * @author Nadissa Lopez 23764
+ * @author Vianka Castro 23201
+ * @author Ricardo Godinez 23247
  */
 public class DriverContenedor {
 
